7c60e30c1582168895dc5d45d805dab2c8ac393c
Evidentsinger14 <cdelancey186423@gmail.com>
2024-12-31 15:07:41 -0500

    implement logic for denying quarterly kit on join.

--- src/main/java/dev/ev1dent/kpmainframe/KPMainFrame.java
+++ src/main/java/dev/ev1dent/kpmainframe/KPMainFrame.java
@@ -1,15 +1,18 @@
 package dev.ev1dent.kpmainframe;
 
 import dev.ev1dent.kpmainframe.events.PlayersFirstJoin;
+import net.luckperms.api.LuckPerms;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class KPMainFrame extends JavaPlugin {
 
+    private LuckPerms luckPerms;
+
     @Override
     public void onEnable() {
+        registerDependencies();
         registerEvents();
-
     }
 
     @Override
@@ -17,7 +20,12 @@ public final class KPMainFrame extends JavaPlugin {
         // Plugin shutdown logic
     }
 
-    public void registerEvents() {
-        Bukkit.getPluginManager().registerEvents(new PlayersFirstJoin(), this);
+    public void registerDependencies() {
+        luckPerms = this.getServer().getServicesManager().load(LuckPerms.class);
     }
+
+    public void registerEvents() {
+        Bukkit.getPluginManager().registerEvents(new PlayersFirstJoin(luckPerms), this);
+    }
+
 }
--- src/main/java/dev/ev1dent/kpmainframe/events/PlayersFirstJoin.java
+++ src/main/java/dev/ev1dent/kpmainframe/events/PlayersFirstJoin.java
@@ -1,18 +1,38 @@
 package dev.ev1dent.kpmainframe.events;
 
+import dev.ev1dent.kpmainframe.utilities.Utils;
+import net.luckperms.api.LuckPerms;
+import net.luckperms.api.model.user.User;
+import net.luckperms.api.node.Node;
+import net.luckperms.api.node.types.PermissionNode;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+
 public class PlayersFirstJoin implements Listener {
+
+    private final LuckPerms luckPerms;
+    private final Utils Utils = new Utils();
+
+    public PlayersFirstJoin(LuckPerms luckPerms) {
+        this.luckPerms = luckPerms;
+    }
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
         Player player = event.getPlayer();
         if(player.hasPlayedBefore()) return;
 
-        // start LP stuff
-        //set essentials.kits.quarterly to false for 30d
+        UUID playerUUID = player.getUniqueId();
+        luckPerms.getUserManager().modifyUser(playerUUID, (User user) -> {
+            Node node = PermissionNode.builder("essentials.kits.quarterly").expiry(30, TimeUnit.DAYS).value(false).build();
+            Utils.logInfo("New Player: %s (%s) - Denying Quarterly Kit", player.getName(), player.getUniqueId());
+            user.data().add(node);
+        });
+
     }
 }
